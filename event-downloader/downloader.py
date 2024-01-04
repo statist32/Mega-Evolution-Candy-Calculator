8a8286b4bdd85203e6c03f713e12ae80416a632b
Lars <lars@Larss-MacBook-Pro.local>
2024-01-04 22:46:52 +0100

    Handle regional forms automatically

--- event-downloader/downloader.py
+++ event-downloader/downloader.py
@@ -44,7 +44,19 @@ def find_all_wild_encounters(
 
 
 def get_pokemon_types(pokemon_name):
-    URL = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower().split(' ')[0]}/"
+    if "Alolan" in pokemon_name:
+        query_pokemon_name = pokemon_name.replace("Alolan ","").split(' ')[0].lower()
+        query_pokemon_name +="-alola"
+    elif "Galarian" in pokemon_name:
+        query_pokemon_name = pokemon_name.replace("Galarian ","").split(' ')[0].lower()
+        query_pokemon_name +="-galar"
+    elif "Hisuian" in pokemon_name:
+        query_pokemon_name = pokemon_name.replace("Hisuian ","").split(' ')[0].lower()
+        query_pokemon_name +="-hisui"
+    else:
+        query_pokemon_name = pokemon_name.lower().split(' ')[0]
+
+    URL = f"https://pokeapi.co/api/v2/pokemon/{query_pokemon_name}/"
     response = requests.get(URL)
     if not response.ok:
         print(f"No types found for {pokemon_name}!\n")
